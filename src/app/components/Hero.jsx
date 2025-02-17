6474b63be3bfa7e9ea3c7a1bc7187cdc84872ea2
Ziyin ab <ziyinab00@gmail.com>
2025-02-17 22:11:57 +0300

    change in hero page in mobile version

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,9 @@ const Hero = () => {
   return (
     <div className='bg-black text-white w-screen text-center overflow-hidden'>
 
-      <div>
+      <div className='flex flex-col justify-center sm:bg-black w-screen items-center '>
+        <div className='  z-50'>
+
       <div className=' flex justify-center text-center'>
         <h3 className=' sm:text-4xl text-2xl text mt-10 sm:mt-20  font-medium'>Tech Fest 2025</h3>
         <Image src={vectorwhite} width="30px" height="30px" alt='vector' className='  sm:w-10 w-8 sm:h-10 h-8 sm:mt-14 mt-7'  /> 
@@ -20,13 +22,14 @@ const Hero = () => {
         <Timer launchDate="2025-02-25T02:00:00"/>
         
         
-        <div className='z-10'>
+       
           
-      <p className='sm:mx-[20%] mx-[8%] font-light text-white sm:text-gray-300 '><span className='font-bold'>More than an event—it's a movement! </span>A place to connect, create, and grow with our tech family. Whether you're starting out or leveling up, this is where innovation meets opportunity!</p>
-       </div>
-      <div className='flex flex-col justify-center sm:bg-black w-screen items-center sm:opacity-100 opacity-70'>
+      <p className='sm:mx-[20%] mx-[8%] font-light text-white sm:text-gray-300 z-50 '><span className='font-bold'>More than an event—it's a movement! </span>A place to connect, create, and grow with our tech family. Whether you're starting out or leveling up, this is where innovation meets opportunity!</p>
+        </div>
+       
+      <div className='flex flex-col justify-center sm:bg-black w-screen items-center ' >
 
-        <Image src={devgif} width={700} height={10} alt='underline' className='  flex  scale-x-150 sm:scale-x-105 scale-150  sm:h-[70vh] sm:w-[70vw] h-[45vh] sm:mt-40 -mt-4 ' /> 
+        <Image src={devgif} width={700} height={10} alt='underline' className='  flex  scale-x-150 sm:scale-x-105 scale-150  sm:h-[70vh] sm:w-[70vw] h-[45vh] sm:mt-40 -mt-2 ' /> 
         <div className='bg-white h-40 -mt-40 w-screen z-10 hidden sm:flex'></div> 
         
       </div>
